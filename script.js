2df59e5b35b35fd51329b6e0d2c75cb43e4d427c
Sarah Peters <sarahnpeters@gmail.com>
2021-03-03 23:34:55 -0600

    adding seven day weather forecast function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+const url = `https://api.openweathermap.org/data/2.5/onecall?lat=39.017582&lon=-94.632797&exclude=minutely&units=imperial&appid=6e3bf53c19fcc30b929ad77a8b75ea60`;
+
 /*
  * function to call weather API
  */
 function getTodayWeather() {
-  const response = UrlFetchApp.fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=39.017582&lon=-94.632797&exclude=minutely&units=imperial&appid=6e3bf53c19fcc30b929ad77a8b75ea60`
-  );
+  const response = UrlFetchApp.fetch(url);
   const responseJson = response.getContentText();
   const data = JSON.parse(responseJson);
 
@@ -21,4 +21,16 @@ function getTodayWeather() {
   todaySheet.getRange(2, 2).setValue([highTemp]);
   todaySheet.getRange(2, 3).setValue([lowTemp]);
   todaySheet.getRange(2, 4).setValue([description]);
+}
+
+function getSevenDay() {
+  const response = UrlFetchApp.fetch(url);
+  const responseJson = response.getContentText();
+  const data = JSON.parse(responseJson);
+  const dailyArray = data.daily;
+  dailyArray.forEach((day) => {
+    const high = Math.round(day.temp.max).toFixed(0);
+    const low = Math.round(day.temp.min).toFixed(0);
+    console.log(high, low);
+  });
 }
